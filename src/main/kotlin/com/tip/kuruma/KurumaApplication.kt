430f3c9805ddfd0a13de4a648784eff120ea5246
Ariel Ramirez <laramirez@prismamp.com>
2023-08-27 23:15:47 -0300

    add project structure

---
+++ src/main/kotlin/com/tip/kuruma/KurumaApplication.kt
@@ -0,0 +1,11 @@
+package com.tip.kuruma
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KurumaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KurumaApplication>(*args)
+}
---
+++ src/test/kotlin/com/tip/kuruma/KurumaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tip.kuruma
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KurumaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
