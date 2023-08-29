a8f4713cfeaa4850a2083b117c279ee093571fa8
Nicolas <rodriguez.n@outlook.com>
2023-08-29 20:59:53 -0300

    Add CarRepository, CarService and CarController: WIP

--- src/main/kotlin/com/tip/kuruma/KurumaApplication.kt
+++ src/main/kotlin/com/tip/kuruma/KurumaApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 
 @SpringBootApplication
-@EntityScan(basePackages = ["com.tip.kuruma"]) // Replace with your entity package
+@EntityScan("com.tip.kuruma")
 class KurumaApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/tip/kuruma/models/Car.kt
+++ src/main/kotlin/com/tip/kuruma/models/Car.kt
@@ -1,4 +1,10 @@
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
 class Car {
+    @Id
+    var id: Long = 0
     var name: String = ""
     var brand: String = ""
     var model: String = ""
