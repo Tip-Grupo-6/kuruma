b243a827bfd4c36bf8c63b8c59e9bf724ab7d1b6
Nicolas <rodriguez.n@outlook.com>
2023-09-07 20:03:11 -0300

    Add CarDTO .Add MaintenanceSchedule. Adjust carController Response using the DTO and wraping values.Skinny Car Model.Add Notification model

---
+++ src/main/kotlin/com/tip/kuruma/models/Notification.kt
@@ -0,0 +1,16 @@
+package com.tip.kuruma.models
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "notification")
+data class Notification(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+    var message: String? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY) // Many notifications can be associated with one car
+    @JoinColumn(name = "car_id")
+    var car: Car? = null
+)
