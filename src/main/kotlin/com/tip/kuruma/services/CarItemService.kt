39b7db80ccd407b72dd7bee5e0b3b2e2c8869182
Nicolas <rodriguez.n@outlook.com>
2023-09-20 19:53:41 -0300

    Add Car Item Controller

--- src/main/kotlin/com/tip/kuruma/services/CarItemService.kt
+++ src/main/kotlin/com/tip/kuruma/services/CarItemService.kt
@@ -26,8 +26,24 @@ class CarItemService(
         return carItemRepository.findById(id).orElse(null)
     }
 
+    fun updateCarItem(id: Long, carItem: CarItem): CarItem {
+        val carItemToUpdate = carItemRepository.findById(id).orElse(null)
+        carItemToUpdate?.let {
+            it.name = carItem.name
+            it.last_change = carItem.last_change
+            it.next_change_due = carItem.next_change_due
+            it.replacement_frequency = carItem.replacement_frequency
+            it.due_status = carItem.due_status
+
+            return carItemRepository.save(it)
+        }
+        return carItem
+    }
+
     fun deleteCarItem(id: Long) {
-        carItemRepository.deleteById(id)
+        val carItemToDelete = carItemRepository.findById(id).orElse(null)
+        val updatedCarItem = carItemToDelete.copy(isDeleted = true)
+        carItemRepository.save(updatedCarItem)
     }
 
 }
