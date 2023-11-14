0a888aa4986d145d2adc0b5f20a85a72494ab366
Ariel Ramirez <laramirez@prismamp.com>
2023-11-13 22:41:10 -0300

    add cors configurations

--- src/main/kotlin/com/tip/kuruma/configuration/CorsConfiguration.kt
+++ src/main/kotlin/com/tip/kuruma/configuration/CorsConfiguration.kt
@@ -1,32 +1,25 @@
 package com.tip.kuruma.configuration
 
-import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import org.springframework.web.server.ServerWebExchange
-import org.springframework.web.server.WebFilter
-import org.springframework.web.server.WebFilterChain
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import reactor.core.publisher.Mono
 
-@Configuration
-class CorsFilter : WebFilter {
-    override fun filter(ctx: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        if (ctx != null) {
-            ctx.response.headers.add("Access-Control-Allow-Origin", "*")
-            ctx.response.headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-            ctx.response.headers.add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
-            if (ctx.request.method == HttpMethod.OPTIONS) {
-                ctx.response.headers.add("Access-Control-Max-Age", "1728000")
-                ctx.response.statusCode = HttpStatus.NO_CONTENT
-                return Mono.empty()
-            } else {
-                ctx.response.headers.add("Access-Control-Expose-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
-                return chain.filter(ctx)
-            }
-        } else {
-            return chain.filter(ctx)
-        }
-    }
+@Component
+class CorsConfig {
+
+//    @Bean
+//    fun corsConfigurer(): WebMvcConfigurer {
+//        return object : WebMvcConfigurer {
+//            override fun addCorsMappings(registry: CorsRegistry) {
+//                registry
+//                        .addMapping("/**")
+//                        .allowedMethods(CorsConfiguration.ALL)
+//                        .allowedHeaders(CorsConfiguration.ALL)
+//                        .allowedOriginPatterns(CorsConfiguration.ALL)
+//                        .allowCredentials(true)
+//            }
+//        }
+//    }
 }
--- src/main/kotlin/com/tip/kuruma/configuration/SecurityConfigFilter.kt
+++ src/main/kotlin/com/tip/kuruma/configuration/SecurityConfigFilter.kt
@@ -4,18 +4,21 @@ import com.tip.kuruma.configuration.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(
+class SecurityConfigFilter(
         val jwtAuthenticationFilter: JwtAuthenticationFilter,
         val authProvider: AuthenticationProvider
 ) {
@@ -23,12 +26,12 @@ class SecurityConfig(
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         return http
-            .csrf { csrf: CsrfConfigurer<HttpSecurity> ->
-                csrf.disable()
-            }
+            .csrf().disable()
+            .cors()
+            .and()
             .authorizeHttpRequests { authRequest ->
                 authRequest
-                    .requestMatchers("/*/**").permitAll()
+                    .requestMatchers("/auth/**").permitAll()
                     .anyRequest().authenticated()
             }
             .sessionManagement { sessionManager: SessionManagementConfigurer<HttpSecurity?> ->
@@ -36,6 +39,19 @@ class SecurityConfig(
             }
             .authenticationProvider(authProvider)
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .httpBasic(Customizer.withDefaults())
             .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("authorization", "content-type", "x-auth-token")
+        configuration.exposedHeaders = listOf("x-auth-token")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
